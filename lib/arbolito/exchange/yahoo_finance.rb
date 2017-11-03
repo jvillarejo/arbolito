b58cda8854a3664cbf9d547103d2addd7227ce75
jvillarejo <arzivian87@gmail.com>
2017-11-03 15:57:54 -0300

    fixed bug

--- lib/arbolito/exchange/yahoo_finance.rb
+++ lib/arbolito/exchange/yahoo_finance.rb
@@ -3,37 +3,37 @@ require 'net/http'
 module Arbolito
   module Exchange
     class YahooFinance
-      def initialize(api_key = '')
-      end
-      # DEPRECATED Yahoo Finance is not supported anymore by Yahoo so use Alpha Vantage API
-      def find_current_rate(quote)
-        warn "[DEPRECATION] Yahoo Finance `find_current_rate` is deprecated.  Please use AlphaVantage `find_current_rate` instead."
-        data = response(build_uri(quote))
+      class << self
+        # DEPRECATED Yahoo Finance is not supported anymore by Yahoo so use Alpha Vantage API
+        def find_current_rate(quote)
+          warn "[DEPRECATION] Yahoo Finance `find_current_rate` is deprecated.  Please use AlphaVantage `find_current_rate` instead."
+          data = response(build_uri(quote))
 
-        values = {
-          quote: quote,
-          price: data[1],
-        }
+          values = {
+            quote: quote,
+            price: data[1],
+          }
 
-        build_rate(values)
-      end
+          build_rate(values)
+        end
 
-      private
-      def response(uri)
-        response = Net::HTTP.get(uri)
-        data = response.gsub(/"|\\n/,'').split(',')
-      end
+        private
+        def response(uri)
+          response = Net::HTTP.get(uri)
+          data = response.gsub(/"|\\n/,'').split(',')
+        end
 
-      def build_uri(quote)
-        param = "#{quote.from.upcase}#{quote.to.upcase}"
-        URI("http://download.finance.yahoo.com/d/quotes.csv?s=#{param}=X&f=nl1d1t1")
-      end
+        def build_uri(quote)
+          param = "#{quote.from.upcase}#{quote.to.upcase}"
+          URI("http://download.finance.yahoo.com/d/quotes.csv?s=#{param}=X&f=nl1d1t1")
+        end
 
-      def build_rate(values)
-        Currency::Rate.new(
-          values[:price],
-          values[:quote].to_hash
-        )
+        def build_rate(values)
+          Currency::Rate.new(
+            values[:price],
+            values[:quote].to_hash
+          )
+        end
       end
     end
   end
--- spec/exchange/yahoo_finance_spec.rb
+++ spec/exchange/yahoo_finance_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper' 
 
 describe Arbolito::Exchange::YahooFinance do 
-  subject(:exchange) { Arbolito::Exchange::YahooFinance.new }
+  subject(:exchange) { Arbolito::Exchange::YahooFinance }
   let(:quote) { Arbolito::Currency::Quote.new('USD' => 'UYU') }
 
   # DEPRECATED Yahoo doesn't support anymore Yahoo Finance so use Alpha Vantage instead
